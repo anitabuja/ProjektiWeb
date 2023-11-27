432418b4938c34275d3f96b20b2d3abc8cb6e28e
erarushiti <er68645@ubt-uni.net>
2023-11-28 00:41:24 +0100

    created single product page and about us page

--- script/script.js
+++ script/script.js
@@ -9,3 +9,33 @@ function validimiLogin() {
     }
     return true;
 }
+
+
+let slideIndex = 1; 
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex = slideIndex + n); 
+}
+
+function currentSlide(n) {  
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide_img");
+
+  if (n > slides.length) {slideIndex = 1} 
+  
+  if (n < 1) {slideIndex = slides.length} 
+  
+  for (i = 0; i < slides.length; i++) {
+  
+    slides[i].style.display = "none";  
+    
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+}
