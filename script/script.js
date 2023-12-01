4af99fc6b689f973a7981be0d3108dbc33ba97bf
anitabuja <bujaanita7@gmail.com>
2023-12-01 22:13:45 +0100

    added some changes

--- script/script.js
+++ script/script.js
@@ -39,3 +39,35 @@ function showSlides(n) {
 
   slides[slideIndex-1].style.display = "block";  
 }
+
+
+function validateForm(){
+  var name=document.getElementById('name').value;
+  var email=document.getElementById('email').value;
+  var psw=document.getElementById('psw').value;
+  var pswRepeat=document.getElementById('pswRepeat').value;
+
+
+  
+  var nameRegex = /^[a-zA-Z\s]+$/;
+  if(!nameRegex.test(name)){
+      alert('please enter a valid name.');
+      return false;
+  }
+
+  var emailRegex = /^[^\s@]+@[^\s@]\.[^\s@]+$/;
+  if(!emailRegex.test(email)){
+      alert('please enter a valid email.');
+      return false;
+  }
+
+  if(psw.length <6){
+      alert('pasword must be at least 6 characters');
+      return false;
+  }
+  if(psw !== pswRepeat){
+      alert('paswords do not match');
+      return false;
+  }
+  return true;
+}
